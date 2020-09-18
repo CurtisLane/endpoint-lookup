c106ce0dcd2a63546328370856f5dd2aa5c8c73e
Curtis Lane <clanelmt@gmail.com>
2020-09-18 18:26:47 -0400

    add search functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Search from './components/Search';
+import locations from './locations.json'
 
 /* The App.js file is acting as our only "Page" component since the funcitonality of the site is so simple. 
 Normally we would set up routes to different pages here using react-router-dom */
 
 function App() {
+
+  const [locationsState, setLocationsState] = useState(locations)
+
+  const handleClick = e => {
+    e.preventDefault()
+    const searchParam = e.target.attributes.getNamedItem("data-value").value
+    const cityStateArr = searchParam.split(', ')
+    const searchResults = locationsState.filter(location => location.city.toLowerCase() === cityStateArr[0].toLowerCase())
+    console.log(searchResults)
+  }
+
   return (
     <div className="App">
-      <Search />
+      <Search handleClick={handleClick} />
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Card, Form } from 'react-bootstrap'
 
 export default function Search({handleClick}) {
 
+    const [valueState, setValueState] = useState('')
+
+    const handleInputChange = e => {
+        setValueState(e.target.value)
+    }
+
     return (
         <>
-            <Card className="col-12 col-sm-10 col-md-8 col-lg-6 offset-sm-1 offset-md-2 offset-lg-3 my-5">
+            <Card className="col-12 col-sm-10 col-md-8 col-lg-6 offset-sm-1 offset-md-2 offset-lg-3 my-5 bg-secondary text-light p-4">
                 <Form>
                     <Form.Group controlId="formBasicSearch">
                         <Form.Label>Search for a city and state in the US.</Form.Label>
-                        <Form.Control type="text" placeholder="Ex: San Francisco, CA" />
+                        <Form.Control onChange={handleInputChange} type="text" placeholder="Ex: San Francisco, CA" />
                     </Form.Group>
-                    <Button variant="primary" type="button" onClick={handleClick}>
+                    <Button data-value={valueState} className="mb-2" variant="primary" type="button" onClick={handleClick}>
                         Search
                 </Button>
                 </Form>
